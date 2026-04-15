a75cf008c7b8d2acb227f232b6ffc21a14cb9c7e
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2026-04-16 07:50:56 +0800

    Add files via upload

---
+++ rwe.py
@@ -0,0 +1,25 @@
+import os
+while 1:
+    try:
+        a = input("Input 1 to exit, type any to test (example: ./sandbox_root/1.txt): ")
+        if a=="1":break
+        try:
+            with open (a,"wb") as f:
+                f.write(b"5")
+            print("sccess W")
+        except Exception as e:
+            print("failed W",e)
+        try:
+            with open (a,"rb") as f:
+                f.read()
+            print("sccess R")
+        except Exception as e:
+            print("failed R",e)
+        try:
+            os.remove(a)
+            print("sccess E")
+        except Exception as e:
+            print("failed E",e)
+    except Exception as e:
+        print(e)
+        break
